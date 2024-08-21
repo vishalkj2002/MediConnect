41f8fb8b7b21495a1b89adcbdd6e0f94dc80c93f
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-08-21 22:44:19 +0530

    Added Services section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import icon03 from '../assets/images/icon03.png';
 import { Link } from 'react-router-dom';
 import {BsArrowRight} from 'react-icons/bs';
 import About from '../components/About/About'
+import ServiceList from '../components/Services/ServiceList';
 
 const Home = () => {
   return (
@@ -112,6 +113,16 @@ const Home = () => {
       </section>
 
       <About/>
+
+      <section>
+        <div className="container">
+          <div className='xl:w-[470px] mx-auto'>
+            <h2 className='heading text-center'>Our medical services</h2>
+            <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+          </div>
+          <ServiceList/>
+        </div>
+      </section>
     </>
   )
 }
