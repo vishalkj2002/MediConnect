7d46bdf194e3f561cb3da20106fcc9399e067861
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-08-23 22:19:12 +0530

    Added doctor's section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -12,6 +12,7 @@ import { Link } from 'react-router-dom';
 import {BsArrowRight} from 'react-icons/bs';
 import About from '../components/About/About'
 import ServiceList from '../components/Services/ServiceList';
+import DoctorList from '../components/Doctors/DoctorList';
 
 const Home = () => {
   return (
@@ -163,6 +164,17 @@ const Home = () => {
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className='xl:w-[470px] mx-auto'>
+            <h2 className='heading text-center'>Our great doctors</h2>
+            <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+          </div>
+
+          <DoctorList/>
         </div>
       </section>
     </>
