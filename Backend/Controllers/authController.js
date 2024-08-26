8587814d8801aab36b762060595dd95d2159b43e
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-08-26 14:46:50 +0530

    Updated backend code for registration, login, reviews and authentication

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -3,15 +3,21 @@ import Doctor from '../models/DoctorSchema.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
+const generateToken = user => {
+    return jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '15d'
+    });
+};
+
 export const register = async(req, res) => {
     const {email, password, name, role, photo, gender} = req.body;
     try {
         let user = null;
 
         if (role === 'patient') {
-            user = User.findOne({email});
+            user = await User.findOne({email});
         } else if (role === 'doctor') {
-            user = Doctor.findOne({email});
+            user = await Doctor.findOne({email});
         }
 
         // Check if user exists
@@ -49,14 +55,44 @@ export const register = async(req, res) => {
 
         res.status(200).json({success: true, message: 'User successfully created'});
     } catch (error) {
-        res.status(500).json({success: true, message: 'User successfully created'});
+        res.status(500).json({success: true, message: 'Internal server error, Try again'});
     }
 }
 
 export const login = async(req, res) => {
+    const {email} = req.body;
     try {
+        let user = null;
 
+        const patient = await User.findOne({email});
+        const doctor = await Doctor.findOne({email});
+
+        if (patient) {
+            user = patient;
+        }
+        if (doctor) {
+            user = doctor;
+        }
+
+        // Check if user exists or not
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        // Compare password
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isPasswordMatch) {
+            return res.status(400).json({status: false, message: 'Invalid credentials'});
+        }
+
+        // Get token
+        const token = generateToken(user);
+
+        const {password, role, appointments, ...rest} = user._doc;
+
+        res.status(200).json({status: true, message: 'Successfully logged in', token, data: {...rest}, role});
     } catch (error) {
-
+        res.status(500).json({status: false, message: 'Failed to login'});
     }
 }
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import mongoose, { connect } from 'mongoose';
-import dotenv from 'dotenv';
-import authRoute from './Routes/auth.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+import authRoute from "./Routes/auth.js";
+import userRoute from "./Routes/user.js";
+import doctorRoute from "./Routes/doctor.js";
+import reviewRoute from "./Routes/review.js";
 
 dotenv.config();
 
@@ -11,36 +14,38 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin: true,
+  origin: true,
 };
 
-app.get('/', (req, res) => {
-    res.send('API is working');
+app.get("/", (req, res) => {
+  res.send("API is working");
 });
 
 // Database Connection
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log('MongoDB database is connected');
-    } catch (error) {
-        console.log('MongoDB database connection failed');
-    }
+    console.log("MongoDB database is connected");
+  } catch (error) {
+    console.log("MongoDB database connection failed");
+  }
 };
-
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use('/api/v1/auth', authRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/doctors", doctorRoute);
+app.use("/api/v1/reviews", reviewRoute);
 
 app.listen(port, () => {
-    connectDB();
-    console.log('Server is running on port ' + port);
+  connectDB();
+  console.log("Server is running on port " + port);
 });
